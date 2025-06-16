a5f4b975901a448a7e46d66721f8276d51e206c8
namtran915 <khanhnamnam67@gmail.com>
2025-06-17 01:23:19 +0700

    index.js

--- index.js
+++ index.js
@@ -387,28 +387,4 @@ client.on('messageCreate', async message => {
     });
   });
 });
-const express = require("express");
-const app = express();
-app.get("/", (req, res) => res.send("Bot is alive!"));
-app.listen(3000, () => console.log("Express keep-alive running"));
-
-const { Client, GatewayIntentBits } = require("discord.js");
-require("dotenv").config();
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
-
-client.once("ready", () => {
-  console.log("🤖 Bot is ready!");
-});
-
-client.on("messageCreate", message => {
-  if (!message.content.startsWith(process.env.PREFIX)) return;
-  const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
-
-  if (command === "ping") {
-    message.channel.send("🏓 Pong!");
-  }
-});
-
 client.login(process.env.TOKEN);
